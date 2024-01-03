8138020f9d1e787d33e63f9eb5544b4ee44a9b73
pranshu <srivastavapranshu2@gmail.com>
2024-01-04 00:27:46 +0530

    removed unnecessary lines

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Layout from "@/components/layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import "@/styles/globals.css";
-Layout;
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
